fb795569e9538f53e897a473f6159c3d3e19cfbc
alvincvms <>
2019-03-28 14:27:08 -0700

    Day 4

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -2,32 +2,48 @@ package section4;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
-	// 1. Make a main method that includes all the steps below…
+	// 1. Make a main method that includes all the steps below.
+	public static void main(String[] args) {
+		
+	
 
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+int random = new Random().nextInt(8);
 	// 3. Print out this variable
+	System.out.println(random);
 
 	// 4. Get the user to enter something that they think is awesome
+	String Awesome = JOptionPane.showInputDialog(null,  "Type in 0,1,2, or 3, and choose one of them out of which one of them you think is awesome");
 
 	// 5. If the random number is 0
-
+	if (random==0) {
 	// -- tell the user whatever they entered is awesome!
-
+	JOptionPane.showMessageDialog(null, Awesome+" is awesome "); 
+	}
 	// 6. If the random number is 1
-
+	if (random==1) {
 	// -- tell the user whatever they entered is ok.
-
+		JOptionPane.showMessageDialog(null, Awesome+" is Ok "); 
+	}
 	// 7. If the random number is 2
-
+		if (random==2) {
+			JOptionPane.showMessageDialog(null, Awesome+" is BORING ");	
 	// -- tell the user whatever they entered is boring.
-
+		JOptionPane.showMessageDialog(null, Awesome+" is BORING ");
+	}
 	// 8. If the random number is 3
-
+			if (random==3) {
+				
+			
 	// -- write your own answer
+				JOptionPane.showMessageDialog(null, Awesome+" is STOOPID ");
+			}
+	
 
 }
-
+}
 
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,18 +1,50 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
+		int score = 0;
 		
 		// 2.  Ask the user a question 
+		String name = JOptionPane.showInputDialog(null, "How many keys are on a standard keyboard?");
 		
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if (name.equals("I am not a nerd")) {
 		// 4.  if the user's answer is correct
-		
+		score = score+1;
 		// -- add one to their score 
+		}
+		String game = JOptionPane.showInputDialog(null, "What is the year of the mac I am using right now?");
+		if (game.equals("Who gives a crap")) {
+		score = score+1;
+		}
+		String tane = JOptionPane.showInputDialog(null, "What month was Benjamin Franklin born in?");
+		if (tane.equals("January")) {
+		score = score+1;
+		}
+		String zane = JOptionPane.showInputDialog(null, "The more you take, the more you leave behind. What am I?");
+		if (zane.equals("Footsteps")) {
+		score = score+1;
+		}
+		String lane = JOptionPane.showInputDialog(null, "How old do you have to be to create your own google account?");
+		if (lane.equals("Does it really matter we all lied")) {
+		score = score+1;
+		}
+		String rane = JOptionPane.showInputDialog(null, "Sorta bonus question: What year was Benjamin Franklin born in?");
+		if (rane.equals("Who cares he is dead")) {
+		score = score+2;
+		}
+		String fane = JOptionPane.showInputDialog(null, "BONUS Question: 159 times 281 no Caculator or paper do it in your head");
+		if (fane.equals("Na bro")) {
+		score = score+3;
+		}
+		
+		JOptionPane.showMessageDialog(null, "Congrats man you got you did not care " + score + " Out of 12 you are good if you got at least 5 points");
+		
 		
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
 		
